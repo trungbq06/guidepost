8f9ce72b5ee597260e4eb2084b9f88ccc69d36f6
Mr Trung <hoangle@Trungs-Mac-mini.local>
2014-10-22 09:52:04 +0700

    Add souce GuidePost

---
+++ GuidePost/GuidePost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuidePost
+//
+//  Created by Mr Trung on 10/22/14.
+//  Copyright (c) 2014 Mr Trung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
